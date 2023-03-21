21dfe87edb150282cc4b9d2b043447947a6fdf97
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-20 19:24:34 -0700

    grabbed all the id attributes and stored them inside a variable, these are the ones im going to need for interactivity

---
+++ script.js
@@ -0,0 +1,16 @@
+// City input and search button 
+var cityInput = $('#cityInput')
+var searchBtn = $('#searchBtn')
+
+
+// grid container Two City (temp, wind, humidty)
+var city = $('#city')
+var temperature = $('#temp')
+var wind = $('#wind')
+var humidity = $('#humidity')
+
+// City History 
+var cityHistory = $('#cityHistory')
+
+// Five Day forecast 
+var fiveForecast = $('#fiveDayForecast')
