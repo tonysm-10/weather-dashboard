d7650510769424f0f3c57bbc8c8ac0517ab68753
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-27 15:14:11 -0700

    did most of the styling already, and made function updateWeatherData(data) do what its supposed to do when the search button is click

--- script.js
+++ script.js
@@ -18,42 +18,62 @@ var fiveForecast = $('#fiveDayForecast')
 // API key for OpenWeatherMap
 var apiKey = '4c83d66135f28f1b211c3a2e9338ac0e'
 
-function getCurrentWeather(cityName) {
-    // API call URL for current weather data
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
-  
-    // Make API call to OpenWeatherMap
-    $.ajax({
-        url: apiUrl,
-        method: "GET",
-        dataType: "json",
-        success: function(data) {
-            console.log(data)
-          // Get and display temperature
-          var temp = data.main.temp;
-          temperature.text("Temperature: " + temp + " °C");
+function getCurrentWeather(cityName, callback) {
+  // API call URL for current weather data
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
+
+  // Make API call to OpenWeatherMap
+  $.ajax({
+    url: apiUrl,
+    method: "GET",
+    dataType: "json",
+    success: function(data) {
+      console.log(data);
+     
+      callback(data);
+
       
-          // Get and display humidity
-          var humid = data.main.humidity;
-          humidity.text("Humidity: " + humid + "%");
-      
-          // Get and display wind speed
-          var windSpeed = data.wind.speed;
-          wind.text("Wind Speed: " + windSpeed + " m/s");
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          console.log(textStatus + ": " + errorThrown);
-        }
-      });
-    }      
+      displayCityInfo(cityName, data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus + ": " + errorThrown);
+    }
+  });
+}
+
+searchBtn.on('click', function() {
+  var cityName = cityInput.val();
+  getCurrentWeather(cityName, function(data) {
+    // Do something with the weather data
+  });
+});
+
   
+ 
+
+  function displayCityInfo(cityName, data) {
+    city.text(cityName);
+    updateWeatherData(data);
+  }
   
-  searchBtn.on('click', function() {
-    var cityName = cityInput.val();
-    displayCityInfo(cityName);
-    displayForecast(cityName);
-    displayCityToLocalStorage(cityName);
-    loadCityHistoryFromLocalStorage();
-  })
+  function updateWeatherData(data) {
+    // Get and display temperature
+    var temp = data.main.temp;
+    temperature.text("🌡️ Temperature: " + temp + " °C");
+  
+    // Get and display humidity
+    var humid = data.main.humidity;
+    humidity.text("💦 Humidity: " + humid + "%");
+  
+    // Get and display wind speed
+    var windSpeed = data.wind.speed;
+    wind.text("🌬️ Wind Speed: " + windSpeed + " m/s");
+
+    
+  }
+// function displayFiveDayForecast(data) {
+  
+// }
+
 
   
