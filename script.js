b5315782326708808c59d5402cd3c9c1378f6db3
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-27 13:03:19 -0700

    In the JavaScript I made the call to the api openWeathermap, I made the call using ajax to return the data for temperature the humidity and windSpeed. I also created and on click for the searchBtn to call the functions displaycityinfo, displayForecast displayCityToLocalStorage and load that ciy history from local storage

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ var cityHistory = $('#cityHistory')
 
 // Five Day forecast 
 var fiveForecast = $('#fiveDayForecast')
+
+// API key for OpenWeatherMap
+var apiKey = '4c83d66135f28f1b211c3a2e9338ac0e'
+
+function getCurrentWeather(cityName) {
+    // API call URL for current weather data
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
+  
+    // Make API call to OpenWeatherMap
+    $.ajax({
+        url: apiUrl,
+        method: "GET",
+        dataType: "json",
+        success: function(data) {
+            console.log(data)
+          // Get and display temperature
+          var temp = data.main.temp;
+          temperature.text("Temperature: " + temp + " °C");
+      
+          // Get and display humidity
+          var humid = data.main.humidity;
+          humidity.text("Humidity: " + humid + "%");
+      
+          // Get and display wind speed
+          var windSpeed = data.wind.speed;
+          wind.text("Wind Speed: " + windSpeed + " m/s");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(textStatus + ": " + errorThrown);
+        }
+      });
+    }      
+  
+  
+  searchBtn.on('click', function() {
+    var cityName = cityInput.val();
+    displayCityInfo(cityName);
+    displayForecast(cityName);
+    displayCityToLocalStorage(cityName);
+    loadCityHistoryFromLocalStorage();
+  })
+
+  
