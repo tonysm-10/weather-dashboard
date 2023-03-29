5f1b3556b86464b012ae22c4d10cff1ae7cc3545
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-03-29 08:36:30 -0700

    I got all the five day forcast to pop up in the boxes.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//date
+var today = $('#date')
+date = dayjs()
+today.text(date.format('MM/DD/YYYY'))
+
 // City input and search button 
 var cityInput = $('#cityInput')
 var searchBtn = $('#searchBtn')
@@ -18,6 +23,8 @@ var fiveForecast = $('#fiveDayForecast')
 // API key for OpenWeatherMap
 var apiKey = '4c83d66135f28f1b211c3a2e9338ac0e'
 
+
+
 function getCurrentWeather(cityName, callback) {
   // API call URL for current weather data
   var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
@@ -32,14 +39,118 @@ function getCurrentWeather(cityName, callback) {
      
       callback(data);
 
-      
+  var lon = data.coord.lon
+  var lat = data.coord.lat
+  
+
+  getFiveDayForecast(lon, lat)
+
       displayCityInfo(cityName, data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus + ": " + errorThrown);
     }
   });
+  
 }
+
+
+function getFiveDayForecast(longitude, latitude) {
+var apiKey = '4c83d66135f28f1b211c3a2e9338ac0e'
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+
+  $.ajax({
+    url: apiUrl,
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+       var temperatureOne = data.list[1].main.temp
+       var humidityOne = data.list[1].main.humidity
+       var windSpeedOne = data.list[1].wind.speed
+       
+      
+console.log(data)
+    getDayOne(temperatureOne, humidityOne, windSpeedOne)
+    getDayTwo(data)
+    getDayThree(data)
+    getDayFour(data)
+    getDayFive(data)
+    displayDates(data)
+    console.log(data.list[1].dt_txt)
+    // getDayFive(data)
+      // process the returned forecast data here
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus + ': ' + errorThrown);
+    }
+  });
+}
+
+function getDayOne(temperature, humidity, windSpeed) {
+  var tempOne = $('#tempOne')
+  var humidOne = $('#humidOne')
+  var windOne = $('#windOne')
+  temper = Math.round(temperature - 273.15)
+  tempOne.text('🌡️ Temperature: ' + temper + ' °C')
+  humidOne.text('💦 Humidity: ' + humidity + "%")
+  windOne.text('🌬️ Wind Speed: ' + windSpeed + " m/s")
+}
+function getDayTwo(data) {
+      var temperature = data.list[9].main.temp
+       var humidity = data.list[9].main.humidity
+       var windSpeed = data.list[9].wind.speed
+       var tempTwo = $('#tempTwo')
+       var humidTwo= $('#humidTwo')
+       var windTwo = $('#windTwo')
+       temperature = Math.round(temperature - 273.15)
+       tempTwo.text('🌡️ Temperature: ' + temperature + ' °C')
+       humidTwo.text('💦 Humidity: ' + humidity + "%")
+       windTwo.text('🌬️ Wind Speed: ' + windSpeed + " m/s")
+}
+
+function getDayThree(data) {
+  var temperature = data.list[17].main.temp
+  var humidity = data.list[17].main.humidity
+  var windSpeed = data.list[17].wind.speed
+  var tempThree = $('#tempThree')
+  var humidThree= $('#humidThree')
+  var windThree = $('#windThree')
+  temperature = Math.round(temperature - 273.15)
+  tempThree.text('🌡️ Temperature: ' + temperature + ' °C')
+  humidThree.text('💦 Humidity: ' + humidity + "%")
+  windThree.text('🌬️ Wind Speed: ' + windSpeed + " m/s")
+}
+
+
+function getDayFour(data) {
+  var temperature = data.list[25].main.temp
+  var humidity = data.list[25].main.humidity
+  var windSpeed = data.list[25].wind.speed
+  var tempFour = $('#tempFour')
+  var humidFour= $('#humidFour')
+  var windFour = $('#windFour')
+  temperature = Math.round(temperature - 273.15)
+  tempFour.text('🌡️ Temperature: ' + temperature + ' °C')
+  humidFour.text('💦 Humidity: ' + humidity + "%")
+  windFour.text('🌬️ Wind Speed: ' + windSpeed + " m/s")
+}
+
+function getDayFive(data) {
+  var temperature = data.list[33].main.temp
+  var humidity = data.list[33].main.humidity
+  var windSpeed = data.list[33].wind.speed
+  var tempFive = $('#tempFive')
+  var humidFive= $('#humidFive')
+  var windFive = $('#windFive')
+  temperature = Math.round(temperature - 273.15)
+  tempFive.text('🌡️ Temperature: ' + temperature + ' °C')
+  humidFive.text('💦 Humidity: ' + humidity + "%")
+  windFive.text('🌬️ Wind Speed: ' + windSpeed + " m/s")
+}
+
+
+
+
 
 searchBtn.on('click', function() {
   var cityName = cityInput.val();
@@ -52,13 +163,14 @@ searchBtn.on('click', function() {
  
 
   function displayCityInfo(cityName, data) {
-    city.text(cityName);
+    city.text(cityName.toUpperCase());
     updateWeatherData(data);
   }
   
   function updateWeatherData(data) {
     // Get and display temperature
     var temp = data.main.temp;
+    temp = Math.round(temp)
     temperature.text("🌡️ Temperature: " + temp + " °C");
   
     // Get and display humidity
@@ -71,9 +183,27 @@ searchBtn.on('click', function() {
 
     
   }
-// function displayFiveDayForecast(data) {
-  
-// }
+
 
 
   
+  //display dates section 
+  function displayDates(data) {
+    var dateOne = $('#dateOne')
+    var dateTwo = $('#dateTwo')
+    var dateThree = $('#dateThree')
+    var dateFour = $('#dateFour')
+    var dateFive = $('#dateFive')
+    // var dateUno = data.list[1].dt_txt
+    // var dateDos = data.list[9].dt_txt
+    // var dateTres = data.list[17].dt_txt
+    // var dateCuatro = data.list[25].dt_txt
+    // var dateCinco = data.list[33].dt_txt
+
+    dateOne.text(data.list[1].dt_txt)
+    dateTwo.text(data.list[9].dt_txt)
+    dateThree.text(data.list[17].dt_txt)
+    dateFour.text(data.list[25].dt_txt)
+    dateFive.text(data.list[33].dt_txt)
+    
+  }
